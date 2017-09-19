21fcd9bda6ec1cfd2a3ab7ce696c9a717624ecf3
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-09-19 22:02:58 +0530

    JUNIT Test for CourseRepository

---
+++ src/test/java/com/in28minutes/database/databasedemo/repository/CourseRepositoryTests.java
@@ -0,0 +1,41 @@
+package com.in28minutes.database.databasedemo.repository;
+
+//import com.in28minutes.database.databasedemo.JpaDemoApplication;
+//import com.in28minutes.database.databasedemo.entity.Course;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+
+import com.in28minutes.database.databasedemo.JpaDemoApplication;
+import com.in28minutes.database.databasedemo.entity.Course;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = JpaDemoApplication.class)
+public class CourseRepositoryTests {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	CourseRepository courseRepository;
+
+	@Test
+	public void findById_basic() {
+		Course course = courseRepository.findById(10001L);
+		//assert("JPA in 50 Steps".equals(course.getName()));
+		logger.info("Tests are Running");
+		Assert.assertEquals("JPA in 50 Steps",course.getName());
+	}
+
+}
