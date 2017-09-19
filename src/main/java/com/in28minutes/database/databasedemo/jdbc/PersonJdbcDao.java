99ec3219589e99208b2e7237c7ecb77ea108d638
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-09-19 15:54:04 +0530

    Completed Leacture 28

---
+++ src/main/java/com/in28minutes/database/databasedemo/jdbc/PersonJdbcDao.java
@@ -0,0 +1,47 @@
+package com.in28minutes.database.databasedemo.jdbc;
+
+import com.in28minutes.database.databasedemo.entity.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+/**
+ * Created by z063407 on 9/19/17.
+ */
+
+@Repository
+public class PersonJdbcDao {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Person> findAll() {
+        return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper(Person.class));
+    }
+
+    public Person findById(int id) {
+        return jdbcTemplate.queryForObject("select * from person where id = ?", new Object[]{id},
+                                            new BeanPropertyRowMapper<Person>(Person.class));
+    }
+
+    public int deleteById(int id) {
+        return jdbcTemplate.update("delete from person where id = ?", new Object[]{id});
+    }
+
+    public int insert(Person person) {
+        return jdbcTemplate.update("insert into person (id, name, location, birth_date )" +
+                "values(?,  ?, ?,?);", new Object[]{person.getId(), person.getName(), person.getLocation(),
+                                       new Timestamp(person.getBirthDate().getTime())});
+    }
+
+    public int update(Person person) {
+        return jdbcTemplate.update("update person set name = ?, location = ?, birth_date = ? "
+                + "where id = ?",
+                new Object[]{person.getName(), person.getLocation(), new Timestamp(person.getBirthDate().getTime()), person.getId()});
+    }
+
+}
