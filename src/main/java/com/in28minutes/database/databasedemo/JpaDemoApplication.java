ceb867d1477234509c2a2c8328638a0cf70c33a1
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-09-19 19:40:41 +0530

    Starting with JPA Entity

--- src/main/java/com/in28minutes/database/databasedemo/JpaDemoApplication.java
+++ src/main/java/com/in28minutes/database/databasedemo/JpaDemoApplication.java
@@ -1,6 +1,5 @@
 package com.in28minutes.database.databasedemo;
 
-import com.in28minutes.database.databasedemo.entity.Person;
 import com.in28minutes.database.databasedemo.jpa.PersonJpaRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Date;
 
 @SpringBootApplication
 public class JpaDemoApplication implements CommandLineRunner{
@@ -26,18 +23,18 @@ public class JpaDemoApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... strings) throws Exception {
-
-
-		logger.info("User ID 10001: {}", personJpaRepository.findById(10001));
-
-//		logger.info("All Users: {}", personJdbcDao.findAll());
-//		logger.info("Deleting 10002: #rows {}",
-//				personJdbcDao.deleteById(10002));
-		logger.info("Inserting 10004: #rows {}",
-				personJpaRepository.insert(new Person("Tara", "Berlin", new Date())));
-		logger.info("Updating 10003: #rows {}",
-				personJpaRepository.update(new Person(10003, "Name 3", "Berlin 3", new Date())));
-		logger.info("All Users: {}", personJpaRepository.findAll());
-		personJpaRepository.deleteById(10002);
+//
+//
+//		logger.info("User ID 10001: {}", personJpaRepository.findById(10001));
+//
+////		logger.info("All Users: {}", personJdbcDao.findAll());
+////		logger.info("Deleting 10002: #rows {}",
+////				personJdbcDao.deleteById(10002));
+//		logger.info("Inserting 10004: #rows {}",
+//				personJpaRepository.insert(new Person("Tara", "Berlin", new Date())));
+//		logger.info("Updating 10003: #rows {}",
+//				personJpaRepository.update(new Person(10003, "Name 3", "Berlin 3", new Date())));
+//		logger.info("All Users: {}", personJpaRepository.findAll());
+//		personJpaRepository.deleteById(10002);
 	}
 }
---
+++ src/main/java/com/in28minutes/database/databasedemo/entity/Course.java
@@ -0,0 +1,7 @@
+package com.in28minutes.database.databasedemo.entity;
+
+/**
+ * Created by z063407 on 9/19/17.
+ */
+public class Course {
+}
