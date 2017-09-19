f5e8f4995e1a8454632d0d83e2c31ff3b6819973
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-09-19 08:19:54 +0530

    Completed Step 22

---
+++ src/main/java/com/in28minutes/database/databasedemo/entity/Person.java
@@ -0,0 +1,53 @@
+package com.in28minutes.database.databasedemo.entity;
+
+import java.util.Date;
+
+/**
+ * Created by z063407 on 9/19/17.
+ */
+public class Person {
+
+    private int id;
+    private String name;
+    private String location;
+    private Date birthDate;
+
+    public Person(int id, String name, String location, Date birthDate) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.birthDate = birthDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
