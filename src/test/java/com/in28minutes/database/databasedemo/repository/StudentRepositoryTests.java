e25c305f6e589f16653b1ba37f6ae775a3f320f4
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-09-22 19:55:06 +0530

    One to Many Relationship - Lazy Fetch - Lecture 69 complete

--- src/test/java/com/in28minutes/database/databasedemo/repository/StudentRepositoryTests.java
+++ src/test/java/com/in28minutes/database/databasedemo/repository/StudentRepositoryTests.java
@@ -21,6 +21,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = JpaDemoApplication.class)
@@ -35,6 +36,7 @@ public class StudentRepositoryTests {
 	EntityManager studentEntityManager;
 
 	@Test
+	@Transactional
 	public void retrieveStudentAndPassportDetails() {
 		Student student = studentEntityManager.find(Student.class, 20001L);
 		logger.info("Student -> {}", student);
