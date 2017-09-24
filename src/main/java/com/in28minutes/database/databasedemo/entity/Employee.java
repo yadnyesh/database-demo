5d58741b052c4f07f3f359d1ab7d887623d51383
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-09-24 20:01:22 +0530

    Created Employee Abstract Class - Lecture 91

---
+++ src/main/java/com/in28minutes/database/databasedemo/entity/Employee.java
@@ -0,0 +1,45 @@
+package com.in28minutes.database.databasedemo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by z063407 on 9/24/17.
+ */
+
+@Entity
+public abstract class Employee {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
