1d39dfa4a0d2c9a3af6671360c5df31e6b352c13
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-09-24 19:58:37 +0530

    Many to Many Relationships - Single Join Table created - Steps 37 - Lecture 90 - complete

--- src/main/java/com/in28minutes/database/databasedemo/JpaDemoApplication.java
+++ src/main/java/com/in28minutes/database/databasedemo/JpaDemoApplication.java
@@ -4,8 +4,6 @@ import com.in28minutes.database.databasedemo.entity.Course;
 import com.in28minutes.database.databasedemo.entity.Student;
 import com.in28minutes.database.databasedemo.repository.CourseRepository;
 import com.in28minutes.database.databasedemo.repository.StudentRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,9 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpaDemoApplication implements CommandLineRunner{
-
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
-
 
 	@Autowired
 	private CourseRepository courseRepository;
@@ -29,12 +24,6 @@ public class JpaDemoApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... strings) throws Exception {
-		//studentRepository.saveStudentWithPassport();
-//		List<Review> reviewList = new ArrayList<>();
-//		reviewList.add(new Review ("5", "Too Good Hands on Stuff"));
-//		reviewList.add(new Review("5", "Hatsoff"));
-//		courseRepository.addReviewsForCourse(10003L, reviewList);
-		//studentRepository.insertHardcodedStudentandCourse();
 		studentRepository.insertStudentandCourse(new Student("Jack and Jill"), new Course("Fooling in 100 Steps"));
 	}
 }
