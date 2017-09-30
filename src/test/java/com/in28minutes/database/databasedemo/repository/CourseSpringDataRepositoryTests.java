d06ab5eafd9b2bddd4e552c290772dd802c19c5e
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-09-30 17:59:02 +0530

    Created PartTimeEmployee Class - Lecture 106 Criteria Queries - Boring and Unuseful

--- src/test/java/com/in28minutes/database/databasedemo/repository/CourseSpringDataRepositoryTests.java
+++ src/test/java/com/in28minutes/database/databasedemo/repository/CourseSpringDataRepositoryTests.java
@@ -1,15 +1,5 @@
 package com.in28minutes.database.databasedemo.repository;
 
-//import com.in28minutes.database.databasedemo.JpaDemoApplication;
-//import com.in28minutes.database.databasedemo.entity.Course;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-
 import com.in28minutes.database.databasedemo.JpaDemoApplication;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
