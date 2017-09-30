d0a179b8a0015af1e3942921dec7daf3d8a301ad
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-09-30 18:14:37 +0530

    CRUD Operations with Spring Data JPA --> Lecture 115

--- src/test/java/com/in28minutes/database/databasedemo/repository/CourseSpringDataRepositoryTests.java
+++ src/test/java/com/in28minutes/database/databasedemo/repository/CourseSpringDataRepositoryTests.java
@@ -2,6 +2,7 @@ package com.in28minutes.database.databasedemo.repository;
 
 import com.in28minutes.database.databasedemo.JpaDemoApplication;
 import com.in28minutes.database.databasedemo.entity.Course;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -28,13 +29,25 @@ public class CourseSpringDataRepositoryTests {
 	@Test
 	public void findById_coursePresent(){
 		Optional<Course> courseOptional = courseRepository.findById(10001L);
+        Assert.assertTrue(courseOptional.isPresent());
 		logger.info("Course Optional --> {}", courseOptional.isPresent());
 	}
 
 	@Test
 	public void findById_courseNotPresent(){
 		Optional<Course> courseOptional = courseRepository.findById(100001L);
+        Assert.assertFalse(courseOptional.isPresent());
 		logger.info("Course Optional --> {}", courseOptional.isPresent());
 	}
+
+    @Test
+    public void playingAroundWithSpringDataRepository(){
+        Course course = new Course("New Microservices in 400 steps");
+	    courseRepository.save(course);
+	    course.setName("400 Steps for Microservices");
+        courseRepository.save(course);
+        logger.info("All Courses -> {}", courseRepository.findAll());
+        logger.info("Total Courses -> {}", courseRepository.count());
+    }
 
 }
