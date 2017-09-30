9c5029edde00e52978628e166ac803ce01b6bd01
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-09-30 18:03:56 +0530

    Created PartTimeEmployee Class - Lecture 114 Spring JPA

--- src/test/java/com/in28minutes/database/databasedemo/repository/CourseSpringDataRepositoryTests.java
+++ src/test/java/com/in28minutes/database/databasedemo/repository/CourseSpringDataRepositoryTests.java
@@ -1,6 +1,8 @@
 package com.in28minutes.database.databasedemo.repository;
 
 import com.in28minutes.database.databasedemo.JpaDemoApplication;
+import com.in28minutes.database.databasedemo.entity.Course;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.persistence.EntityManager;
+import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = JpaDemoApplication.class)
@@ -21,5 +24,17 @@ public class CourseSpringDataRepositoryTests {
 
 	@Autowired
 	EntityManager em;
+
+	@Test
+	public void findById_coursePresent(){
+		Optional<Course> courseOptional = courseRepository.findById(10001L);
+		logger.info("Course Optional --> {}", courseOptional.isPresent());
+	}
+
+	@Test
+	public void findById_courseNotPresent(){
+		Optional<Course> courseOptional = courseRepository.findById(100001L);
+		logger.info("Course Optional --> {}", courseOptional.isPresent());
+	}
 
 }
